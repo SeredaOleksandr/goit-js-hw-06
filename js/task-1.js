c4bc8697d588ead5d0c814369e07d352f030d016
SeredaOleksandr <154976460+SeredaOleksandr@users.noreply.github.com>
2024-04-20 22:03:43 +0300

    Finished HW6

--- js/task-1.js
+++ js/task-1.js
@@ -1,58 +1,34 @@
-// const getUserNames = (users) => users.map((user) => user.name);
+const customer = {
+  username: 'Mango',
+  balance: 24000,
+  discount: 0.1,
+  orders: ['Burger', 'Pizza', 'Salad'],
+  // Change code below this line
+  getBalance() {
+    return this.balance;
+  },
 
-// console.log(
-//   getUserNames([
-//   {
-//     name: "Moore Hensley",
-//     email: "moorehensley@indexia.com",
-//     balance: 2811
-//   },
-//   {
-//     name: "Sharlene Bush",
-//     email: "sharlenebush@tubesys.com",
-//     balance: 3821
-//   },
-//   {
-//     name: "Ross Vazquez",
-//     email: "rossvazquez@xinware.com",
-//     balance: 3793
-//   },
-//   {
-//     name: "Elma Head",
-//     email: "elmahead@omatom.com",
-//     balance: 2278
-//   },
-//   {
-//     name: "Carey Barr",
-//     email: "careybarr@nurali.com",
-//     balance: 3951
-//   },
-//   {
-//     name: "Blackburn Dotson",
-//     email: "blackburndotson@furnigeer.com",
-//     balance: 1498
-//   },
-//   {
-//     name: "Sheree Anthony",
-//     email: "shereeanthony@kog.com",
-//     balance: 2764
-//   },
-// ])
-// ); // ["Moore Hensley", "Sharlene Bush", "Ross Vazquez", "Elma Head", "Carey Barr", "Blackburn Dotson", "Sheree Anthony"]
+  getDiscount() {
+    return this.discount;
+  },
 
+  setDiscount(value) {
+    this.discount = value;
+  },
 
-"use strict";
+  getOrders() {
+    return this.orders;
+  },
 
-const library = {
-	books: 1923,
-	logBookCount() {
-		console.log(this.books);
-	}
+  addOrder(cost, order) {
+    this.balance -= cost - cost * this.discount;
+    this.orders.push(order);
+  },
+  // Change code above this line
 };
 
-function showBooks(callback) {
-	callback()
-}
-
-
-showBooks(library.logBookCount.bind(library));
+customer.setDiscount(0.15);
+console.log(customer.getDiscount()); // 0.15
+customer.addOrder(5000, "Steak");
+console.log(customer.getBalance()); // 19750
+console.log(customer.getOrders()); // ["Burger", "Pizza", "Salad", "Steak"]
